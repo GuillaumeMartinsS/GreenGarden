9ce6d2d212eb03fbdd1cea748bbf444d0b3bbf18
GuillaumeMartinsS <guillaume_martins@sfr.fr>
2024-02-01 22:13:30 +0100

    entity Garden done

--- migrations/Version20240201211103.php
+++ migrations/Version20240201211103.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240201205843 extends AbstractMigration
+final class Version20240201211103 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,12 @@ final class Version20240201205843 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE garden (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE garden');
     }
 }
