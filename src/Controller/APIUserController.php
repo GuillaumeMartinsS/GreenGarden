2f00b36686263bf156cc6536ca29eb9b47ee1039
GuillaumeMartinsS <guillaume_martins@sfr.fr>
2024-02-09 23:02:05 +0100

    Sending a mail once user registered done

--- src/Controller/APIUserController.php
+++ src/Controller/APIUserController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +17,7 @@ class APIUserController extends AbstractController
      * Route to create a new user
      * @Route("/api/users", name="api_user_create", methods={"POST"})
      */
-    public function createPlant(EntityManagerInterface $entityManager, Request $request): Response
+    public function createPlant(EntityManagerInterface $entityManager, Request $request, MailerInterface $mailer): Response
     {
         $data = $request->getContent();
         $dataDecoded = json_decode($data);
@@ -30,6 +32,20 @@ class APIUserController extends AbstractController
 
         $entityManager->persist($newUser);
         $entityManager->flush();
+
+        // Sending an email once user registered
+        $email = (new Email())
+        ->from('hello@example.com')
+        ->to($newUser->getEmail())
+        //->cc('cc@example.com')
+        //->bcc('bcc@example.com')
+        //->replyTo('fabien@example.com')
+        //->priority(Email::PRIORITY_HIGH)
+        ->subject('You just registered to our app')
+        ->text('Welcome ' . $newUser->getName() . '! You just registered to out app ! You can now create your first garden and plant your first seed.')
+        ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
 
         return $this->json(
             $newUser,
